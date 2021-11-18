2c1bb61055158d6e92f048140e2dae79913f5265
Rudra Pratap Singh <72557287+Skylord0071@users.noreply.github.com>
2021-11-18 09:00:32 +0530

    Update 1.py
    Now just write the name of youtube video it gets downloaded

--- 1.py
+++ 1.py
@@ -1,18 +1,30 @@
+import pywhatkit
 from pytube import YouTube
 
-#ask for the link from user
-link = input("Enter the link of YouTube video you want to download:  ")
-yt = YouTube(link)
 
-#Showing details
-print("Title: ",yt.title)
-print("Number of views: ",yt.views)
-print("Length of video: ",yt.length)
-print("Rating of video: ",yt.rating)
-#Getting the highest resolution possible
-ys = yt.streams.get_highest_resolution()
+while True:
+    a = input("Which video do you want sir... ")
+    url = pywhatkit.playonyt((a), open_video=False)
 
-#Starting download
-print("Downloading...")
-ys.download()
-print("Download completed!!")
+    g = input("Sir do want a youtube video link ??(y/n)")
+    if g == 'y':
+        print(url)
+
+    yt = YouTube(url)
+    #Showing details
+    print("Title: ",yt.title)
+    print("Number of views: ",yt.views)
+    print("Length of video: ",yt.length)
+    print("Rating of video: ",yt.rating)
+    #Getting the highest resolution possible
+    ys = yt.streams.get_highest_resolution()
+
+    #Starting download
+    print("Downloading...")
+    ys.download()
+    print("Download completed!!")
+
+    sir = input("Do you want to download again? (y/n)")
+    if sir.lower() == 'n':
+        print("Thank you sir")
+        break
